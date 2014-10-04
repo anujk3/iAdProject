c2b264e64696a77957c7546ad3e46c4ae6ff986c
Anuj Katiyal <anujk3@gmail.com>
2014-10-05 04:54:37 +0530

    Added NSLog to see when the delegate methods are being called. Also when to show and remove the ads from the view.

--- iAdProject/ViewController.m
+++ iAdProject/ViewController.m
@@ -27,26 +27,36 @@
 }
 
 - (void)bannerViewWillLoadAd:(ADBannerView *)banner  NS_AVAILABLE_IOS(5_0){
-    
+    NSLog(@"Ad Banner will load ad.");
 }
 
 
 - (void)bannerViewDidLoadAd:(ADBannerView *)banner{
-    
+    NSLog(@"Ad Banner did load ad.");
+    [UIView animateWithDuration:0.5 animations:^{
+        self.adBanner.alpha = 1.0;
+    }];
 }
 
 
 - (void)bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error{
-    
+    NSLog(@"Unable to show ads. Error: %@", [error localizedDescription]);
+    // Hide the ad banner.
+    [UIView animateWithDuration:0.5 animations:^{
+        self.adBanner.alpha = 0.0;
+    }];
 }
 
 
-- (BOOL)bannerViewActionShouldBegin:(ADBannerView *)banner willLeaveApplication:(BOOL)willLeave { 
+- (BOOL)bannerViewActionShouldBegin:(ADBannerView *)banner willLeaveApplication:(BOOL)willLeave {
+    NSLog(@"Ad Banner action is about to begin.");
     
+    return YES;
 }
 
 - (void)bannerViewActionDidFinish:(ADBannerView *)banner{
-    
+    NSLog(@"Ad Banner action did finish");
+
 }
 
 
